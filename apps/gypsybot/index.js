4d81a8c59b0b84fc2767ed7256513b8f3196b4b7
tewksbum <marcus@mobywize.com>
2016-12-12 01:36:01 -0500

    wired up w/ utu

--- apps/gypsybot/index.js
+++ apps/gypsybot/index.js
@@ -2,10 +2,26 @@ var alexa = require('alexa-app');
 var readlineSync = require('readline-sync');
 var request = require('request');
 var deasync = require('deasync');
+var utuClient = require('utu');
+
+var appId = 'amzn1.ask.skill.314479d6-a797-45cf-8f59-28d17c663fa4';
 
 var app = new alexa.app('gypsybot');
+var utu = new utuClient.Client('7673f5d1298f4c6596458d8dca5a5968');
 
 app.launch(function(req,res) {
+  var user = {
+    platform: utuClient.constants.ALEXA,
+    platformId: appId,
+    userId: req.userId,
+    sessionId: req.sessionId,
+    values: {
+      firstSeen: new Date(),  // can this be a set once value?
+      lastSeen: new Date(),
+    },
+  };
+  utu.user(user);
+  console.log('user: ', user);
   res.say('What sunsign would you like a horoscope for?  And for the day, week, month, or year?')
      .reprompt('Please say a sunsign followed by day, week, month, or year.')
      .shouldEndSession(false)
@@ -26,13 +42,33 @@ app.intent('getHoroscope', {
 }, function(req,res) {
   var sign = req.slot('SunSign');
   var hday = req.slot('Hday') || '';
-  // console.log('have sign: ', sign, ' and day: ', hday);
+  utu.message({
+    platform: utuClient.constants.ALEXA,
+    platformId: appId,
+    userId: req.userId,
+    sessionId: req.sessionId,
+    values: {
+      message: `${sign} ${hday}`,
+      rawMessage: {
+        text:  `${sign} ${hday}`,
+      },
+      botMessage: false,
+    },
+  });
   if (sign) {
     var message = '';
     sign = sign.replace(/,\s*$/, '');
-    // console.log('calling getHoroscope...');
+    utu.event("Asked for Horoscope", {
+      platform: utuClient.constants.ALEXA,
+      platformId: appId,
+      userId: req.userId,
+      sessionId: req.sessionId,
+      values: {
+        "sign": sign,
+        "type": hday,
+      },
+    });
     var h = getHoroscope(sign, hday);
-    // console.log('horoscope: ', h);
     if (!h.error) {
       var greet = '';
       switch (h.day) {
@@ -48,15 +84,47 @@ app.intent('getHoroscope', {
         default:
           greet = `Today's, ${h.date}, `;
       }
+      utu.event("Gave Horoscope", {
+        platform: utuClient.constants.ALEXA,
+        platformId: appId,
+        userId: req.userId,
+        sessionId: req.sessionId,
+        values: {
+          "horoscope": h.horoscope,
+        },
+      });
       message = `${greet} horoscope for ${h.sunsign} is... "${h.horoscope}"`;
+      utu.message({
+        platform: utuClient.constants.ALEXA,
+        platformId: appId,
+        userId: req.userId,
+        sessionId: req.sessionId,
+        values: {
+          message: message,
+          rawMessage: {
+            text: message,
+          },
+          botMessage: true,
+        },
+      });
     } else {
       message = "Sorry, I can't find a horoscope for that sunsign.  Try again`?";
+      utu.event("Horoscope error: ", {
+        platform: utuClient.constants.ALEXA,
+        platformId: appId,
+        userId: req.userId,
+        sessionId: req.sessionId,
+        values: {
+          "sign": sign,
+          "type": hday,
+        },
+      });
     }
   } else {
     message = 'Try again?  Please say a sunsign followed by day, week, month, or year.';
   }
   res.say(message).shouldEndSession(false);
-} );
+});
 
 app.intent('AMAZON.CancelIntent', {
   'slots': {},
@@ -78,6 +146,13 @@ app.intent('AMAZON.HelpIntent', {
   'slots': {},
   'utterances': []
 }, function(req,res) {
+  utu.event("Asked for help", {
+    platform: utuClient.constants.ALEXA,
+    platformId: appId,
+    userId: req.userId,
+    sessionId: req.sessionId,
+    values: {},
+  });
   message = `GypsyBot is a simple horoscope bot.  If you tell it your sunsign
   and then if you want the horoscope for today, the week, month, or year, GypsyBot
   will tell you your horoscope.`;
@@ -90,6 +165,15 @@ app.intent('exit', {
     '{quit|exit|thanks|bye|enough}'
   ]
 }, function(req,res) {
+  utu.event("Exited", {
+    platform: utuClient.constants.ALEXA,
+    platformId: appId,
+    userId: req.userId,
+    sessionId: req.sessionId,
+    values: {
+      endTime: new Date(),
+    },
+  });
   res.say('Goodbye.').shouldEndSession(true);
 });
 
@@ -116,11 +200,9 @@ function getHoroscope(sign, hday) {
     default:
   }
   var url = 'http://horoscope-api.herokuapp.com/horoscope/' + myDate + '/' + sign;
-  // console.log('url: ', url);
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       body = JSON.parse(body)
-      // console.log('body json: ', body);
       horoscope = {
         day: myDate,
         date: body.date,
